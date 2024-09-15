957703baace6b3036ff844b21c16aa6243479846
r-yvan <yvankiliye.rubuto@gmail.com>
2024-09-15 22:55:19 +0200

    Commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,52 +1,10 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { Text, View } from "react-native";
+import "../global.css";
 
-const Home = () => {
+export default function Index() {
   return (
-    <View>
-      <Text>
-        Home Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti
-        tempore explicabo consequuntur impedit ducimus placeat, cumque maiores
-        repellendus ut perferendis sapiente quas commodi voluptates animi
-        repellat error consectetur et at vitae. Fugit accusamus temporibus
-        officia quae, illo excepturi eveniet recusandae dignissimos ex quidem
-        delectus sed ducimus aliquid soluta, quasi provident autem consequuntur
-        alias distinctio quis earum esse, minima aspernatur. Quo, sequi,
-        eligendi commodi odio amet distinctio aut rem exercitationem, provident
-        quasi ex nulla suscipit tempore error dolor vero labore illo a modi
-        blanditiis consectetur iusto! Laboriosam, necessitatibus maxime nesciunt
-        enim vel expedita quos vitae accusantium consequatur explicabo aliquam
-        laborum amet unde voluptate fuga doloremque nam ad nemo? Doloribus
-        cupiditate facere fuga dignissimos reiciendis culpa deserunt ea, nemo
-        voluptas sint eius ut amet consectetur neque aliquam voluptatibus porro
-        ratione corrupti odio dolores consequatur quisquam dolorem quae qui?
-        Molestias a asperiores excepturi at eveniet officiis nihil, aspernatur
-        odit quis illo quos dicta, et illum ipsum sapiente ex non? Beatae totam
-        itaque eos tenetur ullam rerum obcaecati ab doloribus. Officiis quod
-        beatae quo explicabo expedita veritatis voluptas voluptate id dolore
-        natus voluptatibus cum, maiores optio quam ad adipisci ex a hic nobis
-        consequatur consequuntur quibusdam? Ea minus soluta pariatur corrupti,
-        laborum earum officiis obcaecati perferendis a ab atque sequi at debitis
-        corporis modi aliquid vero quidem doloribus exercitationem blanditiis
-        adipisci? Nostrum obcaecati vero molestias nihil, natus ducimus harum
-        aut laudantium expedita voluptatem iure architecto veritatis blanditiis
-        optio esse porro odio quaerat ipsum non nisi fugit, quia consequuntur?
-        Culpa rerum earum odio blanditiis sunt dignissimos recusandae incidunt a
-        eos praesentium ipsum facilis, doloremque architecto veritatis eum? Ex
-        dolores rem cum, explicabo harum ut. Provident libero assumenda
-        laboriosam ea deserunt, est ipsum temporibus delectus. Non repudiandae
-        totam delectus. Aut nesciunt, hic quisquam sapiente, nostrum odio velit
-        fugiat sed placeat veritatis possimus laboriosam praesentium non eius
-        officia facere ipsam magnam. Molestiae aliquid dolor a sit explicabo
-        quae adipisci impedit, placeat asperiores deserunt sint odit ut quasi
-        aspernatur quidem laborum? Deserunt voluptates facilis repellendus
-        consequatur quidem minima corporis eum excepturi mollitia a autem veniam
-        optio ut adipisci atque eaque suscipit expedita, delectus at ratione
-        pariatur nostrum itaque tenetur? Autem minus illum non, dolor
-        voluptatibus vitae itaque corrupti.
-      </Text>
+    <View className="bg-blue-300">
+      <Text className="font-body text-3xl">Dashboard</Text>
     </View>
   );
-};
-
-export default Home;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['Geist-Regular'],
+      },
+    },
   },
   plugins: [],
-}
+};
