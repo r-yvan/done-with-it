18d4e575c2c58362cd6f0e6fc1981f07f4ba24be
r-yvan <yvankiliye.rubuto@gmail.com>
2025-01-03 00:58:30 +0200

    Commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,63 +1,59 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { StyleSheet } from "react-native";
+import { View, Text } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View>
+      <Text>
+        Hello World Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Accusamus consectetur repellendus quae neque quaerat architecto
+        praesentium, sunt cumque et quod hic sequi reprehenderit officia! Sed
+        accusantium molestiae alias ipsa adipisci hic, optio suscipit nesciunt
+        voluptatum amet repellat dolorem, quae veritatis necessitatibus tenetur
+        rerum nam molestias quo placeat dicta? Minima eveniet voluptatibus
+        dignissimos aliquam tenetur non molestiae ea. Laborum rem eos, eaque
+        consequuntur aliquid dolor beatae est adipisci aliquam odio qui a
+        quidem, dolore amet voluptate cupiditate, commodi consectetur nam
+        tempore aperiam. Earum non praesentium molestiae ut veritatis cum, fugit
+        tempore ex inventore amet ullam consequatur facilis aspernatur corrupti
+        odit dolorum in esse iste blanditiis quisquam provident placeat, nemo
+        voluptates consectetur? Labore non eius aperiam dolores, a natus, dolor
+        atque sunt debitis laboriosam magni porro dolore qui. Aliquam fuga fugit
+        ad minus nulla quidem quis, dolorem est ratione, quasi, aspernatur
+        voluptate? Tempore rerum ipsam perferendis minima impedit facilis error
+        velit aliquam corrupti totam! Nam eveniet iste, deserunt, pariatur
+        voluptas quae praesentium error recusandae eaque maiores quas
+        repellendus. Blanditiis sapiente nostrum aliquam numquam, vero alias,
+        voluptate eos accusantium quis, eveniet facilis ratione. Quae minima
+        reiciendis molestiae, adipisci a nemo vero veritatis odio facilis
+        impedit velit porro eaque dolorum in repellat. Ut impedit architecto
+        deserunt laborum quis harum nemo aliquid? Debitis enim quidem iste quod
+        est nobis alias consequuntur hic inventore eum vitae molestias
+        temporibus, beatae, qui quae repellendus delectus ducimus nulla
+        praesentium corrupti distinctio, ipsum modi excepturi! Quo provident,
+        molestiae eaque in libero totam possimus. Saepe consequatur excepturi
+        delectus. Maiores quae ullam, voluptatum, possimus architecto nesciunt
+        earum ducimus mollitia sunt, obcaecati ab fugit optio error iste
+        veritatis. Voluptates repellat, quidem similique explicabo laboriosam
+        delectus reprehenderit eos consequatur tempora nobis eius atque suscipit
+        necessitatibus consequuntur omnis non maiores corporis? Magni similique
+        ad modi, inventore consequuntur iure esse commodi necessitatibus cumque
+        voluptate sapiente error. Earum, ut architecto ab minus cum, ad deleniti
+        eum quia perferendis odit doloribus perspiciatis vel vero quam enim
+        nulla repudiandae voluptas aut mollitia magnam harum eaque ipsa atque!
+        Perspiciatis aut tenetur odit facilis sequi repellat voluptatem
+        similique magni et enim. Cum molestias earum, doloribus reiciendis
+        tempora ipsum? Assumenda ducimus doloremque eius, culpa, eum eaque unde
+        alias nam, facilis iste enim!
+      </Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -69,6 +65,6 @@ const styles = StyleSheet.create({
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
 });
---
+++ components/FlatlistDemo.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import React from "react";
+import * as ImagePicker from "expo-image-picker";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  InputText,
+} from "react-native";
+
+const FlatlistDemo = () => {
+  const requestingPermission = async () => {
+    const result = await ImagePicker.requestCameraRollPermissionsAsync();
+    console.log(result);
+  };
+
+  useEffect(() => {
+    requestingPermission();
+  }, []);
+  
+  const [people, setPeople] = useState([
+    { name: "Uwabera Virginie", id: 1 },
+    { name: "Niyigena Marie Merci", id: 2 },
+    { name: "Umuhire Pacifique", id: 3 },
+    { name: "Mutuyimana Marie France", id: 4 },
+    { name: "Irasubiza Noella", id: 5 },
+    { name: "Rubuto Yvan", id: 6 },
+    { name: "Umuhire Pacifique", id: 7 },
+    { name: "Mutuyimana Marie France", id: 8 },
+    { name: "Irasubiza Noella", id: 9 },
+    { name: "Rubuto Yvan", id: 10 },
+    { name: "Umuhire Pacifique", id: 11 },
+    { name: "Mutuyimana Marie France", id: 12 },
+    { name: "Irasubiza Noella", id: 13 },
+    { name: "Rubuto Yvan", id: 14 },
+  ]);
+
+  const deletePeople = (id) => {
+    setPeople((prevPeople) => {
+      return prevPeople.filter((person) => person.id != id);
+    });
+  };
+
+  return (
+    <View className="justify-center items-center h-96 pt-16">
+      <FlatList
+        className=""
+        keyExtractor={(item) => item.id}
+        data={people}
+        renderItem={({ item }) => (
+          <View className="bg-grey-3 mb-5 p-3 rounded-2xl flex flex-row justify-between">
+            <Text className="text-white text-xl">
+              {item.id} {item.name}
+            </Text>
+            <TouchableOpacity
+              className="bg-red-600 p-1 rounded-lg"
+              onPress={() => deletePeople(item.id)}
+            >
+              <Text className="font-bold text-white">Delete</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+export default FlatlistDemo;
